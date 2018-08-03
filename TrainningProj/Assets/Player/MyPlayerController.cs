f98c39041474341e60dc5bbad92eb4f0580c1b7d
ivo.capelo <ivo.capelo@miniclip.com>
2018-08-03 17:37:42 +0100

    Restarted imp

---
+++ TrainningProj/Assets/Player/MyPlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerController : MonoBehaviour {
+
+    Rigidbody2D _body;
+
+	private void Awake()
+	{
+        _body = GetComponent<Rigidbody2D>();
+	}
+
+	[SerializeField] float mJumpHeight = 0.00001f;
+    [SerializeField] float mMoveSpeed = 1.0f;
+
+    public void Jump() {
+        _body.AddForce(Vector2.up*mJumpHeight, ForceMode2D.Impulse);
+    }
+
+
+    public void Move(float horizontalDir)
+	{
+        /*
+        Vector2 pos2D = transform.position;
+        Vector2 newPos = pos2D + Time.deltaTime * horizontalDir * Vector2.right;
+        _body.MovePosition(newPos);*/
+        _body.AddForce(horizontalDir * Vector2.right*mMoveSpeed, ForceMode2D.Force);
+	}
+}
---
+++ TrainningProj/Assets/Player/PlayerInputReader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputReader : MonoBehaviour {
+
+    MyPlayerController mpc = null;
+    bool mJumping = false;
+
+	private void Start()
+	{
+        mpc = GetComponent<MyPlayerController>();
+	}
+
+	void Update () {
+        if(mpc == null) {
+            return;
+        }
+
+        mJumping |= Input.GetKey(KeyCode.Space);
+            
+	}
+
+    void FixedUpdate()
+    {
+        if (mpc == null)
+        {
+            return;
+        }
+
+        if (mJumping)
+        {
+            mpc.Jump();
+            mJumping = false;
+        }
+
+        mpc.Move(Input.GetAxis("Horizontal"));
+    }
+}
